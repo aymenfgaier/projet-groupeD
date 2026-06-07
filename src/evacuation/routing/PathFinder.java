41e89ec69116e2582f7f2156239dd5b39a85ca97
aymenfgaier <aymenfgaier10@gmail.com>
2026-06-07 18:48:30 +0200

    Delete src/evacuation/routing directory

--- src/evacuation/routing/PathFinder.java
+++
@@ -1,9 +0,0 @@
-package evacuation.routing;
-
-import evacuation.graph.Node;
-import java.util.List;
-
-public interface PathFinder {
-    List<Node> calculerChemin(Node source, Node destination);
-    void recalculer();
-}
